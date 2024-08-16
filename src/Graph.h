52023f0b1b94bef46d276102782d0aa137363828
Jack Boyce <jboyce@gmail.com>
2024-08-16 14:30:52 -0700

    cleanup

--- src/Graph.h
+++ src/Graph.h
@@ -90,6 +90,8 @@ class Graph {
 //------------------------------------------------------------------------------
 
 // Compute (a choose b).
+//
+// The number of states (vertices) in juggling graph (b,h) is (h choose b).
 
 constexpr std::uint64_t Graph::combinations(unsigned a, unsigned b) {
   if (a < b)
@@ -121,42 +123,45 @@ constexpr std::uint64_t Graph::shift_cycle_count(unsigned b, unsigned h,
   return (val / h);
 }
 
-// Compute the number of ways of building states for a single-period graph.
+// Compute the number of states for a single-period graph.
 //
-// We partition each state into `l` slots, where slot `i` is associated with
-// positions i, i + l, i + 2*l, ... in the state, up to a maximum of h - 1.
-// The only degree of freedom is how many objects to put into each slot; the
-// state positions must be filled from the bottom up in order to be part of a
-// period `l` pattern.
+// Not every state in graph (b,h) can be part of a pattern of period `l`. In
+// particular we have the restriction that within the state, position[i] >=
+// position[i + l] for all i.
+//
+// To count states, we partition each state into `l` slots, where slot `i` is
+// associated with positions i, i + l, i + 2*l, ... in the state, up to a
+// maximum of h - 1. The only degree of freedom is how many objects to put into
+// each slot; the state positions must be filled from the bottom up in order to
+// be part of a period `l` pattern.
 
 constexpr std::uint64_t Graph::ordered_partitions(unsigned b, unsigned h,
     unsigned l) {
+  if (l == 0)
+    return 0;
   std::vector<std::uint64_t> options((b + 1) * l, 0);
 
-  for (unsigned pos = l - 1; ; --pos) {
+  // calculate the number of ways to fill slots `pos` and higher using `left`
+  // balls, working backward from the end
+  for (unsigned pos = l; pos-- > 0; ) {
+    // upper bound on number of balls in slot `pos`
+    unsigned max_fill = 0;
+    for (unsigned i = pos; i < h; i += l) {
+      ++max_fill;
+    }
+
     for (unsigned left = (pos == 0 ? b : 0); left <= b; ++left) {
-      // calculate the number of way to fill slots `pos` and higher, using
-      // `left` balls
-
-      unsigned index = pos + left * l;  // index into array
-      unsigned max_fill = 0;
-      for (unsigned i = pos; i < h; i += l) {
-        ++max_fill;
-      }
-      max_fill = std::min(max_fill, left);
-
+      const unsigned index = pos + left * l;  // index into array
       if (pos == l - 1) {
-        options.at(index) = (left <= max_fill ? 1 : 0);
+        options.at(index) = (left <= max_fill ? 1 : 0);  // last slot
       } else {
-        for (unsigned i = 0; i <= max_fill; ++i) {
-          unsigned index2 = (pos + 1) + (left - i) * l;
+        for (unsigned i = 0; i <= max_fill && i <= left; ++i) {
+          // put `i` balls into slot `pos`
+          const unsigned index2 = (pos + 1) + (left - i) * l;
           options.at(index) += options.at(index2);
         }
       }
     }
-
-    if (pos == 0)
-      break;
   }
 
   return options.at(b * l);  // pos = 0, left = b
